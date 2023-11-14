19c27629449f265cda813c46fbab6fb792e87bdf
oussema bouzaida <oussemabouzaida@gmail.com>
2023-11-14 08:26:16 +0000

    intital commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tn</title>
+</head>
+<?php
+include 'navbar.html'
+?>
+<body>
+    <?php
+    include 'sign-up.html'
+    ?>
+
+</body>
+</html>
